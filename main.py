238e621af86c3bf3b91ac9601ec47aaf71cbc83e
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2023-01-21 15:40:07 +0700

    update response date

--- main.py
+++ main.py
@@ -39,15 +39,13 @@ def check_all_messages(message):
     response('Of course... why not, you are very kind hearted', ['do', 'want', 'you', 'to', 'friends', 'with', 'me', 'be'], required_words=['want', 'to', 'friends', 'be'])
     response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
     response('I am chatbots made with complicated logic', ['who', 'are', 'you', 'human', 'animal'], required_words=['are', 'you'])
-    response('I know, today\'s date: ' + today.strftime("%d %B, %y"), ['what', 'date', 'today', 'is'], required_words=['date', 'today'])
+    response('I know, today\'s date: ' + today.strftime("%d %B %Y"), ['what', 'date', 'today', 'is'], required_words=['date', 'today'])
     response('What a good time to have fun, because it\'s ' + now.strftime("%H:%M") + ' o\'clock now', ['what', 'is', 'it', 'time'], required_words=['what', 'time', 'is', 'it'])
 
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
     response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
     
     best_match = max(highest_prob_list, key=highest_prob_list.get)
-    # print(highest_prob_list)
-    # print(f'Best match = {best_match} | Score: {highest_prob_list[best_match]}')
 
     return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
